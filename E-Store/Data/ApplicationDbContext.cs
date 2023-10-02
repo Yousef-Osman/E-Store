82f544f530e2a59d3b0067c432480e914a3f4985
yousef-osman <yousef.osman.31@gmail.com>
2023-10-02 11:49:02 +0300

    Add project files.

---
+++ E-Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Store.Data;
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
