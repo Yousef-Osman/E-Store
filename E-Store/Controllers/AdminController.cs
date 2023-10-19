c4913308d82fd6d927602f01d1d61886c79b4d3b
Yousef Osman <yousef.osman.31@gmail.com>
2023-10-19 14:31:01 +0300

    updating ui

---
+++ E-Store/Controllers/AdminController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Store.Controllers;
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
